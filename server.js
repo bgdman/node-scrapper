b715eaf2fb6a45c293f3616cd796228feaf36b5a
bgdman <bogdan.manolea84@gmail.com>
2021-10-24 20:21:49 +0300

    remove unused comments

--- server.js
+++ server.js
@@ -1,34 +1,21 @@
-// const express = require("express");
 import express from "express";
 import axios from "axios";
 import cheerio from "cheerio";
-import { compareDocumentPosition } from "domutils";
 
 const app = express();
 const port = 3000;
 
-// Add headers before the routes are defined
 app.use(function (req, res, next) {
-  // Website you wish to allow to connect
   res.setHeader("Access-Control-Allow-Origin", "*");
-
-  // Request methods you wish to allow
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
-
-  // Request headers you wish to allow
   res.setHeader(
     "Access-Control-Allow-Headers",
     "X-Requested-With,content-type"
   );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
   res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
   next();
 });
 
